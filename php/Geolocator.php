0181625194f3212f81f1d8da584544fa78b81314
Giuseppe Burtini <git@refrozen.com>
2011-05-10 16:42:48 -0700

    Variable function for case setting (because one library returns Vancouver, while the others return VANCOUVER).

--- php/Geolocator.php
+++ php/Geolocator.php
@@ -1,10 +1,7 @@
 <?php
-
-	// should support standardizing case.
-
-	require_once "Geolibrary.php";
-	require_once "libraries/Maxmind_city.php";
-	require_once "libraries/Geoplugin.php";
+  require_once "Geolibrary.php";
+  require_once "libraries/Maxmind_city.php";
+  require_once "libraries/Geoplugin.php";
   require_once "libraries/Ipinfodb.php";
 
   class Geolocator {
@@ -12,24 +9,35 @@
                           'geoplugin',
                           'ipinfodb');
 
-		var $classes = array();
-		function Geolocator($priority=null) {
-			if($priority !== null) $this->priority = $priority;
-			foreach($this->priority as $option) {
-				$className = ucfirst($option);
-				$this->classes[$option] = new $className();
-			}
-		}
+  	var $classes = array();
+    var $settings = array("case"=>"ucfirst");
+   	function Geolocator($priority=null, $settings=null) {
+   		if($priority !== null) $this->priority = $priority;
+        if($settings !== null) $this->settings = $settings;
 
-		function getCity($ip=null) {
-			if($ip === null) $ip = $_SERVER['REMOTE_ADDR'];
-	
-			foreach($this->priority as $choice) {
-				if($this->classes[$choice]->isAvailable()) {
-					$result = $this->classes[$choice]->getCity($ip);
-					if($result !== false) return $result;
-				}
-			}			
-		}
+   		foreach($this->priority as $option) {
+   			$className = ucfirst($option);
+   			$this->classes[$option] = new $className();
+   		}
+   	}
+
+   	function getCity($ip=null) { return $this->get($ip, "getCity");  }
+    function getCountryCode($ip=null) { return $this->get($ip, "getCountryCode"); }
+    function getCountryName($ip=null) { return $this->get($ip, "getCountryName"); }
+    function getRegion($ip=null) { return $this->get($ip, "getRegion"); }
+
+    function get($ip, $classMethod = "getCity") {
+        if($ip === null) $ip = $_SERVER['REMOTE_ADDR'];
+
+        foreach($this->priority as $choice) {
+            if($this->classes[$choice]->isAvailable()) {
+                $result = $this->classes[$choice]->$classMethod($ip);
+                if($result !== false) {
+                    if($this->settings['case'] != "") $result = $this->settings['case']($result);
+                    return $result;
+                }
+            }
+        }
+    }
   }
 ?>
