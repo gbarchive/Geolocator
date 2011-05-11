8d356b24dfa52b561303959cbf3da2349a1a1efa
Giuseppe Burtini <git@refrozen.com>
2011-05-10 17:08:26 -0700

    Fixing tab stops.

--- php/Geolocator.php
+++ php/Geolocator.php
@@ -1,32 +1,33 @@
 <?php
-  require_once "Geolibrary.php";
-  require_once "libraries/Maxmind_city.php";
-  require_once "libraries/Geoplugin.php";
-  require_once "libraries/Ipinfodb.php";
+    require_once "Geolibrary.php";  
+    require_once "libraries/Maxmind_city.php";
+    require_once "libraries/Geoplugin.php";
+    require_once "libraries/Ipinfodb.php";
 
-  class Geolocator {
+    class Geolocator {
     var $priority = array('maxmind_city',
-                          'ipinfodb', 
-                          'geoplugin');
+                            'ipinfodb',
+                            'geoplugin');
 
-  	var $classes = array();
+    var $classes = array();
     var $settings = array("case"=>"ucfirst", 
                             "checkAvailable"=>true, 
                             "cache"=>true,
                             "debug"=>false);
-   	function Geolocator($priority=null, $settings=null) {
-   		if($priority !== null) $this->priority = $priority;
+
+    function Geolocator($priority=null, $settings=null) {
+        if($priority !== null) $this->priority = $priority;
         if($settings !== null) $this->settings = array_merge($this->settings, $settings);
    		
         foreach($this->priority as $option) {
             $this->_debug("Instantiating {$option}.");
 
-   			$className = ucfirst($option);
-   			$this->classes[$option] = new $className();
-   		}
-   	}
+            $className = ucfirst($option);
+            $this->classes[$option] = new $className();
+        }
+    }
 
-   	function getCity($ip=null) { return $this->get($ip, "getCity");  }
+    function getCity($ip=null) { return $this->get($ip, "getCity");  }
     function getCountryCode($ip=null) { return $this->get($ip, "getCountryCode"); }
     function getCountryName($ip=null) { return $this->get($ip, "getCountryName"); }
     function getRegion($ip=null) { return $this->get($ip, "getRegion"); }
